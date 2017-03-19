c61798a9915a4942de3b108df1ebdf27927ef0b3
Daivd Wang <davidwangnoi@163.com>
2017-03-19 23:03:49 +0800

    ahahahaha

---
+++ dictionary.h
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <map>
+
+namespace Dict{
+	
+}
+
--- possibility.h
+++ possibility.h
@@ -7,168 +7,171 @@
 #include <cmath>
 #include <map>
 
-enum pyPossiModelType{
-	e_pinyinPossi = 0,
-	e_characterPossi,
-	e_2pinyinPossi,
-	e_2characterPossi,
-	e_possiModelTypeCount,
-	e_testPossi
-};
+namespace Possi{
 
-// a class hold the possibility of a certain things in certain field.
-// static int Total is declared in each concrete class
-template<pyPossiModelType type>
-class pyPossiModel{
-public:
-	pyPossiModel() = default;
-	~pyPossiModel() = default;
+	enum PossiModelType{
+		e_pinyinPossi = 0,
+		e_characterPossi,
+		e_2pinyinPossi,
+		e_2characterPossi,
+		e_possiModelTypeCount,
+		e_testPossi
+	};
 
-	// Add this one to all this field
-	void Add(){
-		Count++;
-		Total++;
-	}
+	// a class hold the possibility of a certain things in certain field.
+	// static int Total is declared in each concrete class
+	template<PossiModelType type>
+	class PossiModel{
+	public:
+		PossiModel() = default;
+		~PossiModel() = default;
 
-	// remove an data of this one to all this field.
-	void Remove(){
-		Count--;
-		Total--;
-	}
-	// return -log(P) = -log(Count / Total)
-	double getPossi() const {
-		return -log((double)Count / (double)Total);
-	}
-
-	// get the Count;
-	int getCount() const {
-		return Count;
-	}
-
-	int getTotal() const {
-		return Total;
-	}
-
-private:
-	int Count;
-	static int Total;
-};
-
-template<pyPossiModelType type>
-int pyPossiModel<type>::Total = 0;
-
-// Tp is the struct solve filed data
-// Model is inherited from pyPossiModel
-// Which is Unique in Field.
-// Singleton model is used
-
-template <typename Tp, pyPossiModelType type>
-class pyPossiField{
-	typedef pyPossiModel<type> Model;
-	typedef std::map<Tp, Model> Map;
-public:
-	static pyPossiField* Instance(){
-		if (_instance == nullptr){
-			_instance = new pyPossiField<Tp, type>();
+		// Add this one to all this field
+		void Add(){
+			Count++;
+			Total++;
 		}
-		return _instance;
-	}
-	// Add an certain data to this field.
-	void Add(const Tp &data);
 
-	// Remove an certain data to this field.
-	void Remove(const Tp &data);
-
-	// return the -log(P) of this data to this field.
-	double getPossi(const Tp &data) const;
-
-	// return total sample of this field
-	int getTotal() const;
-
-	// return Count of certain data of this field
-	int getCount(const Tp &data) const;
-
-	// remove all element with possibility than threshold, -log(P) > -log(threshold);
-	void removeElementsLessThan(double threshold);
-private:
-	pyPossiField() = default;
-	~pyPossiField() = default;
-	Map mp;
-	static pyPossiField* _instance;
-};
-
-template <typename Tp, pyPossiModelType type>
-pyPossiField<Tp, type>* pyPossiField<Tp, type>::_instance = nullptr;
-
-template <typename Tp, pyPossiModelType type>
-inline void pyPossiField<Tp, type>::Add(const Tp &data){
-	auto it = mp.find(data);
-	if (it == mp.end()){
-		auto res = mp.insert(std::make_pair(data, Model()));
-
-		// unsuccessfully add
-		if (!res.second){
-			fprintf(stderr, "%s\n", "pyPossiField::ADDERROR");
-			throw std::string("pyPossiField::ADDERROR");
+		// remove an data of this one to all this field.
+		void Remove(){
+			Count--;
+			Total--;
 		}
-		res.first -> second.Add();
-		return;
-	}
-	it -> second.Add();
-}
-
-template <typename Tp, pyPossiModelType type>
-inline void pyPossiField<Tp, type>::Remove(const Tp &data){
-	auto it = mp.find(data);
-	if (it == mp.end()){
-		auto res = mp.insert(std::make_pair(data, Model()));
-
-		// unsuccessfully add
-		if (!res.second){
-			fprintf(stderr, "%s\n", "pyPossiField::ADDERROR");
-			throw std::string("pyPossiField::ADDERROR");
+		// return -log(P) = -log(Count / Total)
+		double getPossi() const {
+			return -log((double)Count / (double)Total);
 		}
-		res.first -> second.Remove();
-		return;
+
+		// get the Count;
+		int getCount() const {
+			return Count;
+		}
+
+		// get Total count of this field
+		int getTotal() const {
+			return Total;
+		}
+
+	private:
+		int Count;
+		static int Total;
+	};
+
+	template<PossiModelType type>
+	int PossiModel<type>::Total = 0;
+
+	// Tp is the struct solve filed data
+	// Model is inherited from PossiModel
+	// Which is Unique in Field.
+	// Singleton model is used
+
+	template <typename Tp, PossiModelType type>
+	class PossiField{
+		typedef PossiModel<type> Model;
+		typedef std::map<Tp, Model> Map;
+	public:
+		static PossiField* Instance(){
+			if (_instance == nullptr){
+				_instance = new PossiField<Tp, type>();
+			}
+			return _instance;
+		}
+		// Add an certain data to this field.
+		void Add(const Tp &data);
+
+		// Remove an certain data to this field.
+		void Remove(const Tp &data);
+
+		// return the -log(P) of this data to this field.
+		double getPossi(const Tp &data) const;
+
+		// return total sample of this field
+		int getTotal() const;
+
+		// return Count of certain data of this field
+		int getCount(const Tp &data) const;
+
+		// remove all element with possibility than threshold, -log(P) > -log(threshold);
+		void removeElementsLessThan(double threshold);
+	private:
+		PossiField() = default;
+		~PossiField() = default;
+		Map mp;
+		static PossiField* _instance;
+	};
+
+	template <typename Tp, PossiModelType type>
+	PossiField<Tp, type>* PossiField<Tp, type>::_instance = nullptr;
+
+	template <typename Tp, PossiModelType type>
+	inline void PossiField<Tp, type>::Add(const Tp &data){
+		auto it = mp.find(data);
+		if (it == mp.end()){
+			auto res = mp.insert(std::make_pair(data, Model()));
+
+			// unsuccessfully add
+			if (!res.second){
+				fprintf(stderr, "%s\n", "PossiField::ADDERROR");
+				throw std::string("PossiField::ADDERROR");
+			}
+			res.first -> second.Add();
+			return;
+		}
+		it -> second.Add();
 	}
-	it -> second.Remove();
-}
 
-template <typename Tp, pyPossiModelType type>
-inline double pyPossiField<Tp, type>::getPossi(const Tp &data) const{
-	auto it = mp.find(data);
-	// not exists
-	if (it == mp.end()) return 0.0f;
-	return it -> second.getPossi();
-}
+	template <typename Tp, PossiModelType type>
+	inline void PossiField<Tp, type>::Remove(const Tp &data){
+		auto it = mp.find(data);
+		if (it == mp.end()){
+			auto res = mp.insert(std::make_pair(data, Model()));
 
-template <typename Tp,pyPossiModelType type>
-inline int pyPossiField<Tp, type>::getCount(const Tp &data) const{
-	auto it = mp.find(data);
+			// unsuccessfully add
+			if (!res.second){
+				fprintf(stderr, "%s\n", "PossiField::ADDERROR");
+				throw std::string("PossiField::ADDERROR");
+			}
+			res.first -> second.Remove();
+			return;
+		}
+		it -> second.Remove();
+	}
 
-	if (it == mp.end()) return 0;
-	return it -> second.getCount();
-}
+	template <typename Tp, PossiModelType type>
+	inline double PossiField<Tp, type>::getPossi(const Tp &data) const{
+		auto it = mp.find(data);
+		// not exists
+		if (it == mp.end()) return 0.0f;
+		return it -> second.getPossi();
+	}
 
-template <typename Tp, pyPossiModelType type>
-inline int pyPossiField<Tp, type>::getTotal() const {
-	// no samples at all
-	if (mp.begin() == mp.end()) return 0;
+	template <typename Tp,PossiModelType type>
+	inline int PossiField<Tp, type>::getCount(const Tp &data) const{
+		auto it = mp.find(data);
 
-	// at least one sample
-	return mp.begin() -> second.getTotal();
-}
+		if (it == mp.end()) return 0;
+		return it -> second.getCount();
+	}
 
-template <typename Tp, pyPossiModelType type>
-inline void pyPossiField<Tp, type>::removeElementsLessThan(double threshold){
-	double mlog = -log(threshold);
-	for (auto it = mp.begin(); it != mp.end(); it++){
-		if (it -> second.getPossi() > mlog){
-			auto j = it;
-			it--;
-			mp.erase(j);
+	template <typename Tp, PossiModelType type>
+	inline int PossiField<Tp, type>::getTotal() const {
+		// no samples at all
+		if (mp.begin() == mp.end()) return 0;
+
+		// at least one sample
+		return mp.begin() -> second.getTotal();
+	}
+
+	template <typename Tp, PossiModelType type>
+	inline void PossiField<Tp, type>::removeElementsLessThan(double threshold){
+		double mlog = -log(threshold);
+		for (auto it = mp.begin(); it != mp.end(); it++){
+			if (it -> second.getPossi() > mlog){
+				auto j = it;
+				it--;
+				mp.erase(j);
+			}
 		}
 	}
-}
-
+} // namespace Possi
 #endif //! POSSI_H
